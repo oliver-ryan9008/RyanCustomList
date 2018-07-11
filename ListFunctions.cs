84a8b0f03b25e4beb9540d0c44c9d41ff43bcf31
oliver.ryan9008 <38868314+rdo1708@users.noreply.github.com>
2018-07-11 14:51:59 -0500

    First commit after corrupted files fixed.

---
+++ ListFunctions.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RyanCustomListProject
+{
+    public class ListFunctions<T>
+    {
+        public int ArrayLength { get; set; }
+        public int ArrayCount { get; set; }
+        public T[] MyArray { get; set; }
+        public ListFunctions()
+        {
+            MyArray = new T[1];
+
+        }
+
+        public void AddList(T elementToAdd)
+        {
+            if (ArrayLength == ArrayCount)
+            {
+                ArrayLength += 1;
+                T[] myNewArray = new T[ArrayLength];
+
+                for (int i = 0; i < ArrayCount; i++)
+                {
+                    myNewArray[i] = MyArray[i];
+                }
+                MyArray = myNewArray;
+            }
+            MyArray[ArrayCount] = elementToAdd;
+            ArrayCount++;
+        }
+
+        public bool RemovedList(T itemToRemove)
+        {
+
+            for (int i = 0; i < ArrayCount; i++)
+            {
+                if (MyArray[i].Equals(itemToRemove))
+                {
+                    ArrayCount--;
+                    MoveArrayIndexes(i);
+                    return true;
+                }
+
+            }
+            return false;
+
+        }
+
+        public void MoveArrayIndexes(int index)
+        {
+            T[] myNewArray = new T[ArrayLength];
+            for (int i = index; i < ArrayCount; i++)
+            {
+                myNewArray[i] = MyArray[i + 1];
+            }
+            myNewArray = MyArray;
+        }
+
+        public int CountArrayLength()
+        {
+            return ArrayLength;
+        }
+
+        public override string ToString()
+        {
+            string convertedStringArray = "";
+            if (ArrayCount != 0)
+            {
+                for (int i = 0; i < ArrayCount; i++)
+                    convertedStringArray = Convert.ToString(MyArray[i]);
+            }
+            return convertedStringArray;
+        }
+    }
+}
---
+++ UnitTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RyanCustomListProject;
+
+namespace RyanCustomListTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethodAddInt()
+        {
+
+            // Arrange
+            ListFunctions<int> listFunctions = new ListFunctions<int>();
+            int elementToAdd = 6;
+            int expected = 8;
+
+            // Act
+            listFunctions.AddList(elementToAdd);
+            listFunctions.AddList(elementToAdd);
+            listFunctions.AddList(elementToAdd);
+            listFunctions.AddList(elementToAdd);
+            listFunctions.AddList(8);
+
+            int actual = listFunctions.MyArray[4];
+
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethodAddString()
+        {
+            ListFunctions<string> listFunctions = new ListFunctions<string>();
+            string elementToAdd = "Hello.";
+            string expected = "Hello.";
+
+            listFunctions.AddList("6");
+            listFunctions.AddList("3");
+            listFunctions.AddList("4");
+            listFunctions.AddList(elementToAdd);
+            listFunctions.AddList("8");
+            string actual = listFunctions.MyArray[3];
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CountArrayLength()
+        {
+            ListFunctions<string> listFunctions = new ListFunctions<string>();
+            int expected = 5;
+
+            listFunctions.AddList("6");
+            listFunctions.AddList("3");
+            listFunctions.AddList("4");
+            listFunctions.AddList("6");
+            listFunctions.AddList("8");
+            int actual = listFunctions.CountArrayLength();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethodRemoveIndex()
+        {
+
+            ListFunctions<int> listFunctions = new ListFunctions<int>();
+            listFunctions.AddList(5);
+            listFunctions.AddList(4);
+            listFunctions.AddList(6);
+            listFunctions.AddList(7);
+            listFunctions.AddList(13);
+            listFunctions.RemovedList(4);
+            int expected = 5;
+
+            int actual = listFunctions.CountArrayLength();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestToStringConverter()
+        {
+            ListFunctions<int> listFunctions = new ListFunctions<int>();
+            string expected = "8";
+            listFunctions.AddList(8);
+
+            string actual = listFunctions.ToString();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
